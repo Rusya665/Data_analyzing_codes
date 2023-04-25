6be153935dae6fa560c5ebb98a2107e67377a22c
Rusya665 <rusya665@gmail.com>
2023-04-25 14:42:43 +0300

    Solar cells IV data plotting

---
+++ Solar_cells_measurements_plotting/main.py
@@ -0,0 +1,15 @@
+from log_creater import LogCreate
+
+
+"""
+Specify the path to the folder with raw data from potentiostat.
+"""
+path = "path/to/your/folder"
+
+
+def main():
+    LogCreate(path)
+
+
+if __name__ == "__main__":
+    main()
